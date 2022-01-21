1510f2d02718ce995111c92383be1992a9997d7e
Cristian1231233 <campagnacristian22@gmail.com>
2022-01-21 16:54:01 +0100

    init footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,103 @@
 <footer>
 
-    <div>footer</div>
+    <footer>
+        <section class="footer-top">
+         <div class="container">
+             
+             <div class="informative">
+             <h2>DC COMIX</h2>
+             <ul>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+             </ul>
+             <h2 class="shop">SHOP</h2>
+             <ul>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+             </ul>
+             </div>
+             <div class="informative">
+             <h2>DC</h2>
+             <ul>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+             </ul>
+             </div>
+             <div class="informative">
+             <h2>SITES</h2>
+             <ul>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+                 <li><a href="#">Characters</a></li>
+             </ul>
+             </div>
+             <div class="logo-big">
+                 <img src="{{asset('images/dc-logo.png')}}" alt="">
+             </div>
+              
+         </div>
+        </section>
+        <section class="footer-button">
+          <div class="container2">
+              <div class="button-social">
+                  <div class="button">
+                    <button>SIGN-UP NOW!</button>
+                  </div>
+                  <div class="socials">
+                      <h1 class="titolo-social">FOLLOW US</h1>
+                     <div class="social">
+                         
+                         
+                        <img src="{{asset('img/footer-facebook.png')}}"
+                        alt="">
+                     </div>
+                     <div class="social">
+                        <img src="{{asset('img/footer-twitter.png')}}"
+                        alt="">
+                     </div>
+                     <div class="social">
+                        <img src="{{asset('img/footer-youtube.png')}}"
+                        alt="">
+                     </div>
+                     <div class="social">
+                        <img src="{{asset('img/footer-pinterest.png')}}"
+                        alt="">
+                     </div>
+                     <div class="social">
+                        <img src="{{asset('img/footer-periscope.png')}}"
+                        alt="">
+                     </div>
+                  </div>
+                  
+              </div>
+          </div>
+        </section>
+        
+    </footer>
 
 </footer>
