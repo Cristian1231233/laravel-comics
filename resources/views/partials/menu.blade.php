92f52baa3b4909198ae3bb1843cbea737f77cc36
Cristian1231233 <campagnacristian22@gmail.com>
2022-01-21 21:07:54 +0100

    commit menu top

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -5,9 +5,22 @@
     <div class="action-comics">
         <div class="text">
             <h2>ACTION COMICS #1000; THE DELUXE EDITION</h2>
+            <div class="green">
+                <div class="one-green">
+                   <div>U.S. PRICE: $19.99</div> 
+                   <div>AVAILABLE</div>
+                </div>
+                <div class="two-green">
+                    Check Availability
+                </div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro expedita, repellendus temporibus quaerat similique atque necessitatibus voluptatibus mollitia, debitis perferendis quibusdam asperiores labore modi magni libero ipsa inventore voluptatem officiis illum est adipisci! Id qui velit commodi laudantium minus! Ipsum, temporibus. Quae, alias. Molestias, blanditiis?</p>
         </div>
         <div class="image">
-
+            <div class="title">ADVERTISEMENT</div>
+            <div>
+                <img src="{{asset('images/adv.jpg')}}" alt="">
+            </div>
         </div>
     </div>
 
