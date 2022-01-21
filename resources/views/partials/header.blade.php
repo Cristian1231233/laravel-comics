0034d8ccb5733daa0abee16ee2d503578f1287a7
Cristian1231233 <campagnacristian22@gmail.com>
2022-01-21 16:21:38 +0100

    init commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container">
+    <div class="logo">
+        <img src="{{asset('images/dc-logo.png')}}" alt="">
+    </div>
+    <div class="menu-link">
+        <nav>
+            <ul>
+                <li>
+                 <a 
+                class="" href=""><strong></strong>
+                </a>
+                <div class="line"></div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+</header>
+
+{{-- <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('about') }}">About</a></li>
+        <li><a href="{{ route('contacts') }}">Contacts</a></li>
+    </ul> --}}
